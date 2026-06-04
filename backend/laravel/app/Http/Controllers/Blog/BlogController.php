f2512d3c711337bc3d56b53309030b823c046b8c
Ignatius Fairlight <lawaierang@gmail.com>
2026-06-04 15:26:30 +0800

    Made scaffold for the whole backend application

---
+++ backend/laravel/app/Http/Controllers/Blog/BlogController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Blog;
+
+use App\Http\Controllers\APIController;
+use Illuminate\Http\Request;
+
+class BlogController extends APIController
+{
+    // Fetch all blogs
+    public function index()
+    {
+
+    }
+
+    // Fetch one blog
+    public function show()
+    {
+
+    }
+
+    // Create one blog
+    public function store()
+    {
+
+    }
+    
+    // Edit blog
+    public function update()
+    {
+
+    }
+
+    // Delete blog
+    public function destroy()
+    {
+
+    }
+}
