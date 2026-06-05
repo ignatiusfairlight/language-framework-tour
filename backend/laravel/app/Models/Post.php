bd798c44758657ec827b843aaeae5f45022fbb92
Ignatius Fairlight <lawaierang@gmail.com>
2026-06-05 09:09:54 +0800

    Added primary keys in `Post` model

--- backend/laravel/app/Models/Post.php
+++ backend/laravel/app/Models/Post.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Table;
 use Illuminate\Database\Eloquent\Model;
 
+#[Table(key: 'id')]
 class Post extends Model
 {
     protected $fillable = [
