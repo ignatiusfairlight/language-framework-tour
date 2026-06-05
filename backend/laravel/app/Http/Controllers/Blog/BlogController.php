603e867565d46f9e5f0d8a0b7527690cc5d6aae3
Ignatius Fairlight <lawaierang@gmail.com>
2026-06-05 09:23:46 +0800

    Updated endpoints for `index` and `show`

--- backend/laravel/app/Http/Controllers/Blog/BlogController.php
+++ backend/laravel/app/Http/Controllers/Blog/BlogController.php
@@ -3,20 +3,26 @@
 namespace App\Http\Controllers\Blog;
 
 use App\Http\Controllers\APIController;
+use App\Services\BlogService;
 use Illuminate\Http\Request;
 
 class BlogController extends APIController
 {
-    // Fetch all blogs
-    public function index()
-    {
+    protected BlogService $blogService;
 
+    public function __construct()
+    {
+        $this->blogService = new BlogService;
     }
 
-    // Fetch one blog
-    public function show()
+    public function index()
     {
-        $id = $req
+        return $this->success($this->blogService->index());
+    }
+
+    public function show(int $id)
+    {
+        return $this->success($this->blogService->show($id));
     }
 
     // Create one blog
--- backend/laravel/app/Services/BlogService.php
+++ backend/laravel/app/Services/BlogService.php
@@ -1,2 +1,38 @@
 <?php
 
+namespace App\Services;
+
+use App\Models\Post;
+
+class BlogService
+{
+    public function index()
+    {
+        return Post::all();
+    }
+
+    public function show(int $id)
+    {
+        return Post::where('id', $id)->first();
+    }
+
+    public function store(string $title, string $content)
+    {
+        $post = Post::create([
+            'title' => $title,
+            'content' => $content
+        ]);
+
+        return "Post created!";
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+}
