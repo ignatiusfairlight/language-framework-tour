a8bde3d7013a8a62083da456a97bde9e4ba9b6a9
Ignatius Fairlight <lawaierang@gmail.com>
2026-06-06 11:59:36 +0800

    Added struct and skeleton form service layer

---
+++ backend/django/blog/schemas/blog.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+
+@dataclass
+class BlogSchema:
+    title: str
+    content: str
---
+++ backend/django/blog/services/blog.py
@@ -0,0 +1,11 @@
+from blog.models.blog import Post
+
+def get_posts
+
+def get_post
+
+def create_post
+
+def edit_post
+
+def delete_post
