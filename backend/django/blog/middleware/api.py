0bbbef05d9f14e92344e2a6b784e2b2bc029d927
Ignatius Fairlight <lawaierang@gmail.com>
2026-06-07 14:33:08 +0800

    Extended `api.py` middleware

--- backend/django/blog/middleware/api.py
+++ backend/django/blog/middleware/api.py
@@ -5,6 +5,7 @@ class ApiResponseMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
+    # Refactor this
     def __call__(self, request):
         response = self.get_response(request)
 
@@ -12,13 +13,19 @@ class ApiResponseMiddleware:
             content_type = response.get('Content-Type', '')
             if 'application/json' not in content_type:
                 return response
-                
-            result = json.loads(response.content)
-            wrapped = {
-                'success' : True if response.status_code == 200 else False,
-                'message' : 'Success' if response.status_code == 200 else 'Fail',
-                'data' : result
-            }
+
+            if request.method == 'GET':
+                result = json.loads(response.content)
+                wrapped = {
+                    'success' : True if response.status_code < 400 else False,
+                    'message' : 'Success' if response.status_code < 400 else 'Fail',
+                    'data' : result
+                }
+            else:
+                wrapped = {
+                    'success' : True if response.status_code < 400 else False,
+                    'message' : 'Success' if response.status_code < 400 else 'Fail',
+                }
             return JsonResponse(wrapped)
 
         return response
