948ee2014d50f7a0f13a1b6c3c5dabaa58f7f25f
Ignatius Fairlight <lawaierang@gmail.com>
2026-06-22 14:50:38 +0800

    Updated `Dockerfile` and `index.ts`.

--- backend/expressjs/index.ts
+++ backend/expressjs/index.ts
@@ -1,10 +1,10 @@
 import express from 'express';
+import routes from './src/routes';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(8006, '0.0.0.0', () => {
     console.log('listening on 8006')
